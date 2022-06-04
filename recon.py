9e335e6a5ed037200843b187ce7c4fa886841f73
Mark Guiang <mahikgot@gmail.com>
2022-06-05 03:09:07 +0800

    now searches for valid packets

--- recon.py
+++ recon.py
@@ -5,17 +5,23 @@ parser.add_argument('filename')
 args = parser.parse_args()
 
 # https://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks/312464#312464
-def chunks(lst, n):
-    """Yield successive n-sized chunks from lst."""
-    for i in range(0, len(lst), n):
-        yield lst[i:i + n]
+def chunks(lst):
+    #yield valid packets, checks for header and footer bits
+    i = 0
+    while i < len(lst) - 15:
+        chunk = lst[i:i + 16]
+        if (chunk[0][1] > 0) and all(x[1] <= 0 for x in chunk[-4:-1]) and (chunk[-1][1] > 0):
+            i += 16
+            yield chunk
+        else:
+            i += 1
 
 times = []
 datas = []
 with open(args.filename, 'r') as f:
     splited = [data.split() for data in f.read().splitlines()[1:]]
     floated = [[float(x), int(y)] for x, y in splited]
-    packets = [packet for packet in chunks(floated, 16)]
+    packets = [packet for packet in chunks(floated)]
     ref = 5/1024
     for packet in packets:
         time =  packet[0][0]
@@ -34,7 +40,7 @@ with open(args.filename, 'r') as f:
             wrongs.append(time)
         times.append(time)
         datas.append(data)
-print(datas)
+
 with open('output.txt', 'w') as f:
     for time, data in zip(times, datas):
         f.write(str(time) + ' ' + str(data) + '\n')
